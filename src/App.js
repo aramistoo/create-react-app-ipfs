f32e94f961ba6cb80856f69da7dd56af55beeb1c
aramistoo <floorads@gmail.com>
2021-12-30 19:01:51 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn More About Fleek!
+          My Crypto Space!
         </a>
       </header>
     </div>
